019928a3e5fd0998a09d1febf4739ae571090a9b
Jon Cockle <yongfook@gmail.com>
2020-07-03 12:20:30 +0800

    update ruby example

--- ruby.rb
+++ ruby.rb
@@ -1,6 +1,6 @@
 #this code contains the placeholders YOUR_API_KEY and YOUR_SIGNED_URL_BASE_ID which are meant to be replaced with your own key / id
 
-#standard example
+#Standard Example
 
 #api_key: your project API key - keep this safe and non-public
 api_key = "YOUR_API_KEY"
@@ -18,4 +18,12 @@ signature = Digest::MD5.hexdigest(api_key + base + query)
 return base + query + "&s=" + signature
 
 
-#base64 example
+#Base64 Example
+#same process as above but the query string will have encoded values
+#plus the &base64=true parameter appended
+
+def b64(string)
+	Base64.urlsafe_encode64(string, :padding => false)
+end
+
+query = "?m[][name]=#{b64("title")}&m[][text]=#{b64("This is a title")}&m[][name]=#{b64("subtitle")}&m[][text]=#{b64("This is a subtitle")}&base64=true"
